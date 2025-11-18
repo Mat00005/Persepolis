4ae9e053a7e3b84cd88c5d730beb735b5e330409
Mat00005 <mateo.reichenbach06@gmail.com>
2025-11-18 17:03:55 +0100

    Final du jour 2

--- src/js/main.js
+++ src/js/main.js
@@ -5,22 +5,82 @@ gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
   // --- PARALLAX INTRO ---
-  gsap.to("#object-left-1", {
+  gsap.to("#object-1", {
     opacity: 0,
-    y: "-20%",
+    y: "-80%",
     scrollTrigger: {
-      trigger: "#object-left-1",
+      trigger: "#object-1",
       start: "top top",
       scrub: 1,
     },
   });
 
-  gsap.to("#object-right-2", {
+  gsap.to("#object-2", {
     opacity: 0,
-    y: "-50%",
+    y: "-80%",
     scrollTrigger: {
-      trigger: "#object-right-2",
-      start: "bottom bottom",
+      trigger: "#object-2",
+      start: "top 10%",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#object-3", {
+    opacity: 0,
+    y: "-80%",
+    scrollTrigger: {
+      trigger: "#object-3",
+      start: "top top",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#object-4", {
+    opacity: 0,
+    y: "-80%",
+    scrollTrigger: {
+      trigger: "#object-4",
+      start: "top top",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#object-5", {
+    opacity: 0,
+    y: "-80%",
+    scrollTrigger: {
+      trigger: "#object-5",
+      start: "top top",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#object-6", {
+    opacity: 0,
+    y: "-80%",
+    scrollTrigger: {
+      trigger: "#object-6",
+      start: "top top",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#object-7", {
+    opacity: 0,
+    y: "-80%",
+    scrollTrigger: {
+      trigger: "#object-7",
+      start: "top top",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#object-8", {
+    opacity: 0,
+    y: "-80%",
+    scrollTrigger: {
+      trigger: "#object-8",
+      start: "top top",
       scrub: 1,
     },
   });
@@ -43,41 +103,73 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: ".zoom-container",
+        trigger: ".zoom-container-1",
         start: "top top",
         scrub: 2,
+        ease: "slow(0.7,0.7,false)",
         pin: true,
       },
     })
-    .to("#zoom-element", {
-      width: 500,
-      height: 500,
+    .to("#zoom-element-1", {
+      scale: 1.2,
+      ease: "none",
+      opacity: 1,
+    });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".zoom-container-2",
+        start: "top top",
+        scrub: 2,
+        ease: "slow(0.7,0.7,false)",
+        pin: true,
+      },
+    })
+    .to("#zoom-element-2", {
+      scale: 2,
+      ease: "none",
+      opacity: 1,
     });
 
   // --- PARALLAX SECTION "ANGRY MAN" ---
-  gsap.to(".angry-man img", {
-    y: "-10%", // image monte
+  gsap.to(".parallax-element-1", {
+    y: "50%",
     ease: "none",
     scrollTrigger: {
-      trigger: ".white", // la section blanche
-      start: "top bottom",
+      trigger: ".parallax-container-1",
+      start: "top top",
       end: "bottom top",
       scrub: true,
-      // markers: true, // à activer pour debug
     },
   });
 
-  gsap.to(".angry-man p", {
-    y: "10%", // texte descend
+  // --- MANIFESTATION TRANSLATE  ---
+  gsap.to(".translate-element-1", {
+    x: "50%",
     ease: "none",
     scrollTrigger: {
-      trigger: ".white",
+      trigger: ".translate-container-1",
       start: "top bottom",
       end: "bottom top",
       scrub: true,
-      // markers: true,
+      opacity: 1,
     },
   });
+
+  gsap.to(".translate-element-2", {
+    x: "-50%",
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".translate-container-1",
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+      opacity: 1,
+    },
+  });
+
+  // --- A PROPOS PARALLAX  ---
 
   // --- CURSEUR PERSONNALISÉ ---
   const cursor = document.querySelector(".cursor");
